dc7511ef5868d6bdd6eae75add4c77e5fb07d8ca
chicken-developer <quynh.fullstackdev@gmail.com>
2021-06-04 20:01:27 +0700

    Add simple filter for dataset

--- src/main/scala/GrowLocation.scala
+++ src/main/scala/GrowLocation.scala
@@ -3,7 +3,7 @@ import java.sql.Date
 
 import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
 import org.apache.spark.sql.functions._
-
+// Serial	Latitude	Longitude	Type	SensorType	Code	BeginTime	EndTime
 object GrowLocation extends App {
 
   val spark = SparkSession.builder()
@@ -11,15 +11,32 @@ object GrowLocation extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
+  case class Location(
+                       Serial: String,
+                       Latitude: Double,
+                       Longitude: Double,
+                       Type: String,
+                       SensorType: String,
+                       Code: String,
+                       BeginTime: String,
+                       EndTime: String
+                     )
+
   val growLocationDF: DataFrame = spark.read
     .format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
     .load("src/main/resources/data/grow_location.csv")
 
-    
-  // TSV
-  growLocationDF.write
+
+  import spark.implicits._
+  val growLocationDS = growLocationDF.as[Location]
+
+  growLocationDS.filter(_.Longitude != 0)
+
+  // map, flatMap, fold, reduce, for comprehensions ...
+
+  growLocationDS.toDF().write
     .format("csv")
     .option("header", "true")
     .option("sep", "\t")
